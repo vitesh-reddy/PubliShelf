6f3387b38b552421a154829eda5a1120192ca55f
vitesh-reddy <k.l.viteshreddy@gmail.com>
2025-03-11 21:16:15 +0530

    reorder files

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # PubliShelf
 Online Interface for Book and Magazine Transactions with Antique Auctions
-
+```
 git add . <br>
 git commit -m "message"<br>
 git push origin main<br>
+```
--- server.js
+++ server.js
@@ -1,26 +1,26 @@
+import dotenv from 'dotenv';
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import mockBuyerData from "./public/mockData/mockBuyerData.js";
+import buyerRouter from "./server/routes/buyerRoutes.js";
+import publisherRoutes from "./server/routes/publisherRoutes.js";
+import signupRouter from "./server/routes/signupRoutes.js";
+import styles from "./public/styles/styles.js";
 console.clear();
 
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-import mockBuyerData from "./public/mockBuyerData.js";
-import buyerRouter from "./server/routes/buyerRoutes.js";
-import publisherRoutes from "./server/routes/publisherRoutes.js";
-import signupRouter from "./server/routes/signupRoutes.js";
-import styles from "./public/css/styles.js";
-
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("index", { books: mockBuyerData, styles: styles });
-});
+app.get("/", (req, res) => res.render("index", { books: mockBuyerData, styles: styles }) );
 
 app.get("/auth/login", (req, res) => res.render("auth/login"));
 
@@ -30,7 +30,4 @@ app.use("/auth/signup", signupRouter);
 app.get("/about", (req, res) => res.render("about", { styles: styles }));
 app.get("/contact", (req, res) => res.render("contact", { styles: styles }));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
