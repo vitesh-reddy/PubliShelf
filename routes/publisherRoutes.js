fc1acb7d8746bb3313a9f0ca2dffc927c0452b55
K_L_Vitesh_Reddy <147067995+vitesh-reddy@users.noreply.github.com>
2025-05-14 19:46:46 +0530

    allowed webp img format to upload

--- routes/publisherRoutes.js
+++ routes/publisherRoutes.js
@@ -21,7 +21,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "publishelf/books", 
-    allowed_formats: ["jpg", "jpeg", "png"], 
+    allowed_formats: ["jpg", "jpeg", "png", "webp"], 
   },
 });
 
